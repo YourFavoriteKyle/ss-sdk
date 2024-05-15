76dfe7093ecad0cd0a32b044c467e99b3b581112
YourFavoriteKyle <20785882+YourFavoriteKyle@users.noreply.github.com>
2024-05-15 15:04:31 -0400

    Add workflow export demo

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
+import json
+
 from src.smartsheet_sdk import Smartsheet
+
+import csv
 
 
 def main():
     sd = Smartsheet()
     sd.headless.login.with_user_pass()
-    sd.headless.workflows.list_workflows(0)
+    workflows = sd.headless.workflows.list_workflows()
+
+    column_names = [
+        "Workflow Name",
+        "Workflow ID",
+        "Enabled",
+        "Referenced Columns",
+        "Contacts",
+    ]
+    workflows_data = []
+
+    for workflow in workflows["data"]["workflows"]:
+        ref_col = [str(c["columnID"]) for c in workflow["sheetColumns"]]
+        ref_contact = [c["emailAddress"] for c in workflows["data"]["userContacts"]]
+        workflows_data.append(
+            [
+                workflow["name"],
+                workflow["id"],
+                workflow["enabled"],
+                ", ".join(ref_col),
+                ", ".join(ref_contact),
+            ]
+        )
+
+    with open("demo_workflows.csv", "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file, delimiter=",")
+        writer.writerow(column_names)
+        for workflow in workflows_data:
+            writer.writerow(workflow)
 
 
 if __name__ == "__main__":
