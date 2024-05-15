3298880aeea69abc6c8754a5238f0884391da36d
YourFavoriteKyle <20785882+YourFavoriteKyle@users.noreply.github.com>
2024-05-15 15:03:50 -0400

    Add columnID mapping to list_workflows

--- src/headless/workflow.py
+++ src/headless/workflow.py
@@ -4,12 +4,13 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from .headless import Headless
 
+import json
 
 class Workflow:
     def __init__(self, headless: Headless):
         self.headless = headless
 
-    def list_workflows(self, permalink: str) -> object:
+    def list_workflows(self, permalink: str, mapped: bool = True) -> object:
         """
         List all workflows and their properties from a sheet.
 
@@ -18,10 +19,20 @@ class Workflow:
         This request will make additional background requests in
         order to map the user accessible sheet permalink to the WebAPI sheetId.
 
+        Warning
+        -------
+        Only columnIDs are currently mapped to their publicAPI ids
+
         Parameters
         ----------
         permalink : str
             The permalink of the sheet
+        mapped : bool
+            Whether or not to map the containerIDs to their publicAPI ids
+        
+        Returns
+        -------
+        json object
         """
 
         url_details = self.headless.get_url_details(permalink)
@@ -32,8 +43,24 @@ class Workflow:
 
         metadata = metadata.json()
 
-        return self.headless.request(
+
+        r = self.headless.request(
             "POST",
             {"formName": "webop", "formAction": "GetWorkflowsBySheet"},
             {"sheetId": metadata["data"]["gatewayResponse"]["body"]["id"]},
         )
+
+        response = r.json()
+
+        if not mapped: 
+            return response
+
+        column_defs = self.headless.columns.get_column_defs(permalink)
+        response_string = json.dumps(response)
+
+        # NOTE: Maps the containerIDs to their publicAPI ids
+        for column_def in column_defs:
+            response_string = response_string.replace(column_def[2], column_def[20])
+        
+        return json.loads(response_string)
+
